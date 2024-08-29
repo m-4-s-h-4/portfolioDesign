668c07e279e92cf04d0f8f0360a05a2d63c44745
Maria <maria123321borovikova@gmail.com>
2024-08-29 16:21:05 +0200

    new grid variants

--- src/components/base components/LayoutComponents/Grid/Grid.tsx
+++ src/components/base components/LayoutComponents/Grid/Grid.tsx
@@ -3,35 +3,41 @@ import styled from "styled-components";
 import Box from "../../Primatives/Box/Box";
 
 export interface GridProps {
-  variant?: "variant1" | "variant2" | "variant3";
+  variant?: "variant1" | "variant2" | "variant3" | "variant4" | "variant5";
   style?: React.CSSProperties;
   children: React.ReactNode;
 }
 
 const variant1GridAreas = [
-  "1 / 1 / 3 / 2", // div1
-  "1 / 2 / 2 / 3", // div2
-  "1 / 3 / 3 / 4", // div3
-  "3 / 1 / 4 / 2", // div4
-  "2 / 2 / 4 / 3", // div5
-  "3 / 3 / 4 / 4", // div6
+  "1 / 1 / 3 / 2",
+  "1 / 2 / 2 / 3",
+  "1 / 3 / 3 / 4",
+  "3 / 1 / 4 / 2",
+  "2 / 2 / 4 / 3",
+  "3 / 3 / 4 / 4",
 ];
 
-const variant2GridAreas = [
-  "1 / 1 / 2 / 2", // div1
-  "1 / 2 / 2 / 3", // div2
-  "1 / 3 / 2 / 4", // div3
-];
+const variant2GridAreas = ["1 / 1 / 2 / 2", "1 / 2 / 2 / 3", "1 / 3 / 2 / 4"];
 
 const variant3GridAreas = [
-  "1 / 1 / 3 / 2", // div1
-  "1 / 2 / 2 / 3", // div2
-  "2 / 2 / 3 / 3", // div3
-  "1 / 3 / 3 / 4", // div4
+  "1 / 1 / 3 / 2",
+  "1 / 2 / 2 / 3",
+  "2 / 2 / 3 / 3",
+  "1 / 3 / 3 / 4",
+];
+
+const variant4GridAreas = [
+  "1 / 1 / 2 / 2", // div1
+  "1 / 2 / 2 / 4", // div2
+];
+
+const variant5GridAreas = [
+  "1 / 1 / 2 / 3", // div1
+  "1 / 3 / 2 / 4", // div2
 ];
 
 const GridContainer = styled(Box)<{
-  variant: "variant1" | "variant2" | "variant3";
+  variant: "variant1" | "variant2" | "variant3" | "variant4" | "variant5";
 }>`
   display: grid;
   width: 100%;
@@ -41,7 +47,9 @@ const GridContainer = styled(Box)<{
       ? "repeat(3, 1fr)"
       : props.variant === "variant2"
         ? "1fr"
-        : "repeat(2, 1fr)"};
+        : props.variant === "variant3"
+          ? "repeat(2, 1fr)"
+          : "1fr"};
   grid-column-gap: 10px;
   grid-row-gap: 10px;
 `;
@@ -49,6 +57,18 @@ const GridContainer = styled(Box)<{
 const GridItem = styled(Box)<{ area: string }>`
   grid-area: ${(props) => props.area};
   overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  > * {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
 
   img {
     width: 100%;
@@ -67,7 +87,11 @@ const Grid: React.FC<React.PropsWithChildren<GridProps>> = ({
       ? variant1GridAreas
       : variant === "variant2"
         ? variant2GridAreas
-        : variant3GridAreas;
+        : variant === "variant3"
+          ? variant3GridAreas
+          : variant === "variant4"
+            ? variant4GridAreas
+            : variant5GridAreas;
 
   return (
     <GridContainer variant={variant} style={style}>
