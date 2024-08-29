e0af105f6baaf27e6fb2afef74657394f2f11d83
Maria <maria123321borovikova@gmail.com>
2024-08-29 16:22:46 +0200

    about section

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 import Heading from "../../components/base components/TypographyComponents/Heading/Heading";
+import Container from "../../components/base components/Container/Container";
+import Button from "../../components/base components/Button/Button";
+import Flex from "../../components/base components/LayoutComponents/Flex/Flex";
+import Box from "../../components/base components/Primatives/Box/Box";
 
 const About: React.FC = () => {
   return (
-    <Heading level="h1" color="dark" align="center">
-      About
-    </Heading>
+    <Container height="auto">
+      <Box paddingBottom="SpacingSpacing10" paddingTop="SpacingSpacing10">
+        <Flex
+          direction="column"
+          xAlign="center"
+          yAlign="center"
+          gap="SpacingSpacing2"
+        >
+          {" "}
+          <Heading
+            level="h3"
+            color="dark"
+            align="center"
+            paddingLeft="SpacingSpacing20"
+            paddingRight="SpacingSpacing20"
+          >
+            Hey, I'm Maria, a freelance designer based in Barcelona! I
+            specialize in motion graphics and UI design. I will help you to
+            transform your static content into dynamic, engaging visuals that
+            elevate your digital presence.
+          </Heading>
+          <Button text="tell me your story" />
+        </Flex>
+      </Box>
+    </Container>
   );
 };
 
