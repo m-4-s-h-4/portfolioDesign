589468f2e33e7dca0bc7e6fe995593d989fceda2
Maria <maria123321borovikova@gmail.com>
2024-09-10 14:35:13 +0200

    about section changes

---
+++ src/components/about components/Services/Services.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import Flex from "../../base components/LayoutComponents/Flex/Flex";
+import Heading from "../../base components/TypographyComponents/Heading/Heading";
+import Paragraph from "../../base components/TypographyComponents/Paragraph/Paragraph";
+import Stack from "../../base components/LayoutComponents/Stack/Stack";
+import { TextColorDark } from "../../../tokens/js/variables";
+
+const CircleImageWrapper = styled.div`
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+`;
+
+const CircleImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 50%;
+`;
+
+interface ServiceProps {
+  imageSrc: string;
+  title: string;
+  text: string;
+}
+
+const ServiceWrapper = styled.div`
+  border-top: 1px solid ${TextColorDark};
+  padding-top: 1rem;
+  width: 100%;
+`;
+
+const Service: React.FC<ServiceProps> = ({ imageSrc, title, text }) => {
+  return (
+    <ServiceWrapper>
+      <Flex xAlign="flex-start" yAlign="flex-start" gap="SpacingSpacing1">
+        <CircleImageWrapper>
+          <CircleImage src={imageSrc} alt={title} />
+        </CircleImageWrapper>
+        <Stack>
+          <Heading color="dark" level={"h4"}>
+            {title}
+          </Heading>
+          <Paragraph textTransform="none">{text}</Paragraph>
+        </Stack>
+      </Flex>
+    </ServiceWrapper>
+  );
+};
+
+export default Service;
--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import styled from "styled-components";
 import Heading from "../../components/base components/TypographyComponents/Heading/Heading";
 import Container from "../../components/base components/Container/Container";
 import Services from "../../components/about components/Services/Services";
 import Stack from "../../components/base components/LayoutComponents/Stack/Stack";
-import Grid from "../../components/base components/LayoutComponents/Grid/Grid"; // Import Grid
+import Grid from "../../components/base components/LayoutComponents/Grid/Grid";
 
 const textLines = [
-  "Hey, I'm Masha,",
-  "A freelance designer ",
-  "Based in Barcelona!",
+  "Hi, I'm Masha,",
+  "A creative designer",
+  "Based in Barcelona.",
 ];
 
 const SquareToRectImage = styled(motion.img)`
@@ -21,24 +22,40 @@ const SquareToRectImage = styled(motion.img)`
 `;
 
 const About: React.FC = () => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.2 });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
   return (
     <Container
       height="auto"
-      paddingLeftRight="SpacingSpacing2"
+      paddingLeftRight="SpacingSpacing6"
       paddingTopBottom="SpacingSpacing10"
       direction="row"
     >
-      <Grid variant="variant5" gridHeight="auto">
-        <Stack gap="SpacingSpacing3">
-          <Heading level="h3" color="dark" align="left">
+      <Grid variant="variant5" gridHeight="auto" gap="SpacingSpacing6">
+        <Stack gap="SpacingSpacing3" ref={ref}>
+          <Heading level="h2" color="dark" align="left">
             {textLines.map((line, index) => (
               <motion.div
                 key={index}
-                initial={{ opacity: 0, y: 30 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                  duration: 1,
-                  delay: index * 0.8,
+                initial="hidden"
+                animate={controls}
+                variants={{
+                  hidden: { opacity: 0, y: 30 },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                      duration: 1,
+                      delay: index * 0.5,
+                    },
+                  },
                 }}
               >
                 {line}
@@ -46,51 +63,68 @@ const About: React.FC = () => {
             ))}
           </Heading>
 
-          {/* Square to Rectangle Image */}
           <SquareToRectImage
             src="https://images.unsplash.com/photo-1526510747491-58f928ec870f?q=80&w=3638&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Profile"
-            initial={{ width: "10rem", height: "18rem" }}
-            animate={{
-              width: "100%",
-              height: "18rem",
-              borderRadius: "40px",
-            }}
-            transition={{
-              duration: 1.5,
-              ease: "easeInOut",
+            initial="hidden"
+            animate={controls}
+            variants={{
+              hidden: { width: "10rem", height: "20rem" },
+              visible: {
+                width: "100%",
+                height: "20rem",
+                borderRadius: "40px",
+                transition: {
+                  duration: 1.5, // Increased duration for smoother animation
+                  ease: [0.42, 0, 0.58, 1], // Smoother cubic-bezier curve
+                  delay: 0.8, // Slightly earlier start to maintain flow
+                },
+              },
             }}
           />
         </Stack>
 
-        <Stack gap="SpacingSpacing3">
-          <Services
-            imageSrc={
-              "https://i.pinimg.com/originals/62/db/a2/62dba2ee08a822e6c5527a85bc7ad8b3.gif"
-            }
-            title={"Motion Graphics"}
-            text={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
-            }
-          />
-          <Services
-            imageSrc={
-              "https://i.pinimg.com/originals/44/3e/35/443e3595e5518592e3753ffa5699657b.gif"
-            }
-            title={"Ui Design"}
-            text={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
-            }
-          />
-          <Services
-            imageSrc={
-              "https://i.pinimg.com/originals/06/18/72/061872a01924df1ee69379cd93b42476.gif"
-            }
-            title={"Web development"}
-            text={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
-            }
-          />
+        <Stack gap="SpacingSpacing3" ref={ref}>
+          {[
+            "Motion Graphics",
+            "Digital Design & Branding",
+            "Web Development",
+          ].map((serviceTitle, index) => (
+            <motion.div
+              key={index}
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: {
+                  opacity: 1,
+                  x: 0,
+                  transition: {
+                    duration: 1,
+                    delay: 1.8 + index * 0.3, // Services start sooner, shorter delay between them
+                  },
+                },
+              }}
+            >
+              <Services
+                imageSrc={
+                  index === 0
+                    ? "https://i.pinimg.com/originals/62/db/a2/62dba2ee08a822e6c5527a85bc7ad8b3.gif"
+                    : index === 1
+                      ? "https://i.pinimg.com/originals/44/3e/35/443e3595e5518592e3753ffa5699657b.gif"
+                      : "https://i.pinimg.com/originals/06/18/72/061872a01924df1ee69379cd93b42476.gif"
+                }
+                title={serviceTitle}
+                text={
+                  index === 0
+                    ? "Create eye-catching animations that bring your ideas to life. Perfect for videos, social media, and more."
+                    : index === 1
+                      ? "Design engaging user interfaces and eye-catching social media posts that captivate your audience and enhance your brand's presence."
+                      : "Build powerful websites that make your brand stand out and perfectly match your unique identity."
+                }
+              />
+            </motion.div>
+          ))}
         </Stack>
       </Grid>
     </Container>
